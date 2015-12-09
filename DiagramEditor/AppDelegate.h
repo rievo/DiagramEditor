d586a0c8d37842aed1b4a846a91105ea07b82427
Diego <diegovm22@gmail.com>
2015-12-09 09:58:51 +0100

    Initial Commit

---
+++ DiagramEditor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DiagramEditor
+//
+//  Created by Diego Vaquero Melchor on 9/12/15.
+//  Copyright © 2015 Diego Vaquero Melchor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DiagramEditor/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DiagramEditor
+//
+//  Created by Diego Vaquero Melchor on 9/12/15.
+//  Copyright © 2015 Diego Vaquero Melchor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
